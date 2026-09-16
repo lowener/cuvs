97d585796b66444c2a80e8ab254e896bed8d5b30
Robert Maynard <rmaynard@nvidia.com>
2026-09-16 13:01:13 -0400

    brute_force_prefiltered test warp boundary but with less input (#2579)
    For the longest running test items in brute_force_prefiltered.cu we can do a tweaks to improve runtime:
    * Use smaller input dim values which aren't a multiple of the vector width to still validate serial trailing loads 
    * Increase the sparsity but keep about the `kSddmmMaxSelectivity` threshold  
    
    These combine together to remove 20 minutes from `NEIGHBORS_TEST` .
    
    Authors:
      - Robert Maynard (https://github.com/robertmaynard)
    
    Approvers:
      - Tamas Bela Feher (https://github.com/tfeher)
    
    URL: https://github.com/NVIDIA/cuvs/pull/2579

--- cpp/tests/neighbors/brute_force_prefiltered.cu
+++ cpp/tests/neighbors/brute_force_prefiltered.cu
@@ -1012,21 +1012,23 @@ const std::vector<PrefilteredBruteForceInputs<index_t>> selectk_inputs = {
   {1024, 8192, 3, 1, 0.1, cuvs::distance::DistanceType::InnerProduct},
   {1024, 8192, 5, 1, 0.1, cuvs::distance::DistanceType::L2SqrtExpanded},
   {1024, 8192, 8, 1, 0.1, cuvs::distance::DistanceType::CosineExpanded},
-  {1024, 8192, 2050, 16, 0.4, cuvs::distance::DistanceType::L2Expanded},
 
-  {1024, 8192, 2051, 16, 0.5, cuvs::distance::DistanceType::L2Expanded},
-  {1024, 8192, 2052, 16, 0.2, cuvs::distance::DistanceType::L2Expanded},
-  {1024, 8192, 2050, 16, 0.4, cuvs::distance::DistanceType::InnerProduct},
-  {1024, 8192, 2051, 16, 0.5, cuvs::distance::DistanceType::InnerProduct},
-  {1024, 8192, 2052, 16, 0.2, cuvs::distance::DistanceType::InnerProduct},
+  {1024, 8192, 34, 16, 0.1, cuvs::distance::DistanceType::L2Expanded},
+  {1024, 8192, 35, 16, 0.15, cuvs::distance::DistanceType::L2Expanded},
+  {1024, 8192, 36, 16, 0.135, cuvs::distance::DistanceType::L2Expanded},
 
-  {1024, 8192, 2050, 16, 0.4, cuvs::distance::DistanceType::L2SqrtExpanded},
-  {1024, 8192, 2051, 16, 0.5, cuvs::distance::DistanceType::L2SqrtExpanded},
-  {1024, 8192, 2052, 16, 0.2, cuvs::distance::DistanceType::L2SqrtExpanded},
-  {1024, 8192, 2050, 16, 0.4, cuvs::distance::DistanceType::CosineExpanded},
-  {1024, 8192, 2051, 16, 0.5, cuvs::distance::DistanceType::CosineExpanded},
+  {1024, 8192, 34, 16, 0.1, cuvs::distance::DistanceType::InnerProduct},
+  {1024, 8192, 35, 16, 0.15, cuvs::distance::DistanceType::InnerProduct},
+  {1024, 8192, 36, 16, 0.135, cuvs::distance::DistanceType::InnerProduct},
 
-  {1024, 8192, 2052, 16, 0.2, cuvs::distance::DistanceType::CosineExpanded},
+  {1024, 8192, 34, 16, 0.1, cuvs::distance::DistanceType::L2SqrtExpanded},
+  {1024, 8192, 35, 16, 0.15, cuvs::distance::DistanceType::L2SqrtExpanded},
+  {1024, 8192, 36, 16, 0.135, cuvs::distance::DistanceType::L2SqrtExpanded},
+
+  {1024, 8192, 34, 16, 0.1, cuvs::distance::DistanceType::CosineExpanded},
+  {1024, 8192, 35, 16, 0.15, cuvs::distance::DistanceType::CosineExpanded},
+  {1024, 8192, 36, 16, 0.135, cuvs::distance::DistanceType::CosineExpanded},
+
   {1024, 8192, 1, 16, 0.5, cuvs::distance::DistanceType::L2Expanded},
   {1024, 8192, 2, 16, 0.2, cuvs::distance::DistanceType::L2Expanded},
 
